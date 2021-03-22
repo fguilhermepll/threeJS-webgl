2920a9548130e4e6b8667085d542613be94770bd
unknown <fguilhermepll@gmail.com>
2021-03-22 11:46:41 -0300

    adding model loading

--- main.js
+++ main.js
@@ -1,34 +1,62 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.123.0/build/three.module.js';
+import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three/examples/jsm/loaders/GLTFLoader.js';
+
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000)
+var camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer({antialias: true});
 var sceneObjects = [];
 
-camera.position.z = 5;
+const controls = new OrbitControls(camera, renderer.domElement);
 
-renderer.setClearColor("#e5e5e5");
-renderer.setSize(window.innerWidth, window.innerHeight);
+function init(){
+  camera.position.z = 5;
+  controls.update();
 
-document.body.appendChild(renderer.domElement);
+  renderer.setClearColor("#e5e5e5");
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-window.addEventListener('resize', () =>{
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
+  document.body.appendChild(renderer.domElement);
 
-    camera.updateProjectionMatrix();
-})
+  window.addEventListener('resize', () =>{
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      camera.aspect = window.innerWidth / window.innerHeight;
 
-addExperimentalCube();
+      camera.updateProjectionMatrix();
+  })
+}
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshLambertMaterial({color: 0xFFCC00});
-var mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-sceneObjects.push(mesh);
+const loader = new GLTFLoader();
+var carObj;
 
-var light = new THREE.PointLight(0xFFFFFF, 1, 500);
-light.position.set(10, 0, 25);
-scene.add(light);
-sceneObjects.push(light);
+function loadingObject(){
+  loader.load( 'firetruck.glb', function ( gltf ) {
+    carObj = gltf.scene;
+    scene.add( gltf.scene );
+    render();
+  
+  }, undefined, function ( error ) {
+  
+    console.error( error );
+  
+  });
+}
+
+function addCube(){
+  var geometry = new THREE.BoxGeometry(1, 1, 1);
+  var material = new THREE.MeshLambertMaterial({color: 0xFFCC00});
+  var mesh = new THREE.Mesh(geometry, material);
+  mesh.position.x = 1;
+  scene.add(mesh);
+  sceneObjects.push(mesh);
+}
+
+function addLight(){
+  var light = new THREE.PointLight(0xFFFFFF, 1, 500);
+  light.position.set(10, 0, 25);
+  scene.add(light);
+  sceneObjects.push(light);
+}
 
 function vertexShader() {
     return `
@@ -69,21 +97,31 @@ function addExperimentalCube() {
     })
   
     let mesh = new THREE.Mesh(geometry, material);
-    mesh.position.x = 2;
+    mesh.position.x = -1;
     scene.add(mesh);
     sceneObjects.push(mesh);
 }
 
-
 var render = function(){
     requestAnimationFrame(render);
     
+    //carObj.rotation.x += 0.01
+    carObj.rotation.y += 0.01
+
     for(let object of sceneObjects) {
         object.rotation.x += 0.01
         object.rotation.y += 0.03
       }
-
+    controls.update();
     renderer.render(scene, camera);
 }
 
-render();
+function main(){
+  init();
+  addExperimentalCube();
+  addCube();
+  loadingObject();
+  addLight();
+}
+
+main();
